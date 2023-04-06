cdc89d3187f59b083a008b9a209500402f8ea212
Tecu23 <andrei.teculescu@gmail.com>
2023-04-06 08:16:22 +0300

    big O notation first concepts

---
+++ coding_interview/javascript/algorithms/index.js
@@ -0,0 +1,40 @@
+/* ----  IMMEDIATE TIME -> O(1) ---- */
+console.log("Welcome to the Data Structures and Algorithms Course");   /* O(1) -> Immediate Time */
+
+
+/* ----  LINEAR TIME -> O(n) ---- */
+
+const large = new Array(50_000).fill('dory');
+
+large[10] = 'nemo';
+
+function findNemo(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO');
+      break;
+    }
+  }
+  let t1 = performance.now(); 
+  console.log(`Call to find Nemo took ${t1 - t0} miliseconds`)
+}
+
+findNemo(large) /* O(n) -> Linear Time */
+
+
+/* ----  QUADRATIC TIME -> O(n^2) ---- */
+
+const boxes = ['a', 'b', 'c', 'd', 'e'];
+
+function loggAllPairsOfArray(array) {
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(array[i], array[j])
+    }
+  }
+}
+loggAllPairsOfArray(boxes) /* O(n^2) -> Quadratic Time */
+
+

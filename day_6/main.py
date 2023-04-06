51a8316c2e4039a7b36f5bf95fd767ffd9ceb91a
Tecu23 <andrei.teculescu@gmail.com>
2023-04-06 09:01:08 +0300

    day 6

---
+++ day_6/main.py
@@ -0,0 +1,16 @@
+# Functions
+def my_function():
+    print("Hello!")
+    print("Hi!")
+
+my_function() # Calling the function
+
+# While Loops
+i = 10
+while i > 0:
+    my_function()
+    print(i)
+    i -= 1
+
+
+ 
